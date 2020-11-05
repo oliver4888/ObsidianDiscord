7b046f7f1b1f8ac8bbc1726633bf42116d79c0a6
Oliver <8377772+oliver4888@users.noreply.github.com>
2020-11-05 20:37:55 +0000

    Add Setup script. Remove hard coded links to the Obsidian install.

---
+++ Setup.ps1
@@ -0,0 +1,24 @@
+﻿Add-Type -AssemblyName System.Windows.Forms
+
+$currentDirectory = Get-Location
+
+Write-Host "Locate and select your Obsidian install..."
+
+$fileBrowser = New-Object System.Windows.Forms.OpenFileDialog
+$fileBrowser.Multiselect = $false
+$fileBrowser.InitialDirectory = $currentDirectory
+$fileBrowser.Filter = "Obsidian.exe|Obsidian.exe"
+
+if($fileBrowser.ShowDialog() -eq "OK")
+{
+    $target = Split-Path -Path $fileBrowser.FileName
+    $link = Join-Path -Path $currentDirectory "Obsidian"
+    cmd /c mklink /J $link $target
+}
+else
+{
+    Write-Host "File browser cancelled. Unable to create symbolic link!" -ForegroundColor Red
+}
+
+Write-Host "Press any key to continue..."
+[System.Console]::ReadKey()
